160a6f6196b7265c1287ce89b434bc3f013e6f6d
purwaren <and.thau@gmail.com>
2020-03-12 10:59:52 +0700

    add authentication from db

--- src/main/java/com/pusilkom/demo/config/SecurityConfig.java
+++ src/main/java/com/pusilkom/demo/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.pusilkom.demo.config;
 
+import com.pusilkom.demo.service.CustomUserDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,6 +13,8 @@ import org.springframework.security.web.access.AccessDeniedHandler;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 //    @Autowired AccessDeniedHandler accessDeniedHandler;
+    @Autowired
+    private  CustomUserDetailService userDetailService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,9 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
-                .and()
-                .withUser("admin").password("password").roles("ADMIN");
+        auth.userDetailsService(userDetailService).passwordEncoder(new Md5PasswordEncoder());
     }
 }
---
+++ src/main/java/com/pusilkom/demo/service/CustomUserDetailService.java
@@ -0,0 +1,34 @@
+package com.pusilkom.demo.service;
+
+import com.pusilkom.demo.dto.table.UsersItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UsersService usersService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UsersItem user = usersService.getUsersItemByUsername(username);
+
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_"+user.getRole().toUpperCase());
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+        authorities.add(grantedAuthority);
+
+        return new User(user.getUsername(), user.getPassword(), authorities);
+    }
+
+
+}
