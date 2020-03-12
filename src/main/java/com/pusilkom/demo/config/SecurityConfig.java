a3fae828564ab6ba2da8a7bff5a3aebc66419bdc
purwaren <and.thau@gmail.com>
2020-03-12 11:03:49 +0700

    change to bcrypt password encoder

--- src/main/java/com/pusilkom/demo/config/SecurityConfig.java
+++ src/main/java/com/pusilkom/demo/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
@@ -37,6 +38,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailService).passwordEncoder(new Md5PasswordEncoder());
+        auth.userDetailsService(userDetailService).passwordEncoder(new BCryptPasswordEncoder());
     }
 }
